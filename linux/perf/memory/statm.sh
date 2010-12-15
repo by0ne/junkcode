641cab60016e1a448d5f44439ec78bcc9aecbbe7
Elia Pinto <yersinia.spiros@gmail.com>
2010-12-15 16:37:54 +0100

    add statm

---
+++ linux/perf/memory/statm.sh
@@ -0,0 +1,24 @@
+cat /proc/"$1"/statm | awk '
+BEGIN { PGSZ=4 ; }
+{
+TOTAL = $1*$PGSZ;
+RSS = $2*$PGSZ;
+SHARED = $3*PGSZ;
+ANON = RSS-SHARED;
+TEXT = $4*$PGSZ;
+LIBS = $5$PGSZ;
+DATA = $6*PGSZ;
+}
+END {
+print "Convert in KBYTE";
+print "TOTAL VM :" TOTAL;
+
+print "Resident :" RSS;
+print "Shared :" SHARED;
+print "Anon :" ANON;
+print "Text :" TEXT;
+print "Shared Libs :" SHARED;
+print "Data :" DATA;
+print " ";
+}
+'
