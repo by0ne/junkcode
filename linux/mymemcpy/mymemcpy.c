77747c541b78e27083ebafc2617b616a50ca2f88
Elia Pinto <yersinia.spiros@gmail.com>
2011-01-21 15:16:24 +0100

    mymemcpy first add

---
+++ linux/mymemcpy/mymemcpy.c
@@ -0,0 +1,17 @@
+#include <sys/types.h>
+#include "config.h"
+
+void *memcpy(void *dst, const void *src, size_t size)
+{
+ void *orig = dst;
+ asm volatile("rep ; movsq"
+  :"=D" (dst), "=S" (src)
+  :"0" (dst), "1" (src), "c" (size >> 3)
+  :"memory");
+ asm volatile("rep ; movsb"
+  :"=D" (dst), "=S" (src)
+  :"0" (dst), "1" (src), "c" (size & 7)
+  :"memory");
+ return orig;
+}
+
